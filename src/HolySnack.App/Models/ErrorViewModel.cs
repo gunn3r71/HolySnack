9ccaa15647911985acba247ec698761187df5a68
Lucas <lucasgunner3@gmail.com>
2021-10-17 20:52:50 -0300

    Adicionar arquivos de projeto.

---
+++ src/HolySnack.App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HolySnack.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
